9fdedba3a0bd302b1a3b5eaed141ee51ce01b70c
JulienMartens <47147929+Streum@users.noreply.github.com>
2022-01-19 11:35:32 +0100

    changed KS test to test both sides separately

--- Classes/ComparatifPopulations.py
+++ Classes/ComparatifPopulations.py
@@ -26,58 +26,66 @@ class ComparatifPopulations():
                 os.mkdir(os.getcwd() + "\\Donnees "+ self.population2.nom + "\\Patients") 
             for i in self.population2.patients:
                 self.generateurDonneesGlobal(i, os.getcwd() + "\\Donnees "+ self.population2.nom + "\\Patients\\" + i.nom)
-            self.resultatsKSTest = self.testKSGlobal(self.population1, self.population2,os.getcwd()+ "\\Donnees "+ self.diffPopulation.nom)
-            self.generateurDonneesGlobal(self.diffPopulation,os.getcwd() + "\\Donnees "+ self.diffPopulation.nom, self.resultatsKSTest)            
+            self.resultatsKSTestPos, self.resultatsKSTestNeg = self.testKSGlobal(self.population1, self.population2,os.getcwd()+ "\\Donnees "+ self.diffPopulation.nom)
+            self.generateurDonneesGlobal(self.diffPopulation,os.getcwd() + "\\Donnees "+ self.diffPopulation.nom, self.resultatsKSTestPos, self.resultatsKSTestNeg )            
 
     def testKSGlobal(self, population1, population2, path):
         if not os.path.exists(path):
             os.mkdir(path)
         workbook = xlsxwriter.Workbook(path + '\\Test Kolmogorov-Smirnov.xlsx')
         worksheet = workbook.add_worksheet()
-        worksheet.write(0,0, "Articulation")
-        worksheet.write(1,0, "Axe")
-        colonne = 1
+
+        ligne = 0
         #Kolmogorov-Smirnov test, hypothèse nulle = p-value > 5% = les 2 pops sont les mêmes
-        resultatTests = {}
+        resultatTestsPos = {}
+        resultatTestsNeg = {}
         for articulation in population1.articulations:
-            worksheet.write(0,colonne, articulation)
-            worksheet.write(1,colonne, "X")
-            worksheet.write(1,colonne+2, "Y")
-            worksheet.write(1,colonne+4, "Z")
+            worksheet.write(ligne,0, "Articulation")
+            worksheet.write(ligne+1,0, "Axe")
+            worksheet.write(ligne+2,0, "KS Test superieur")
+            worksheet.write(ligne+3,0, "KS Test inférieur")
+            worksheet.write(ligne,1, articulation)
+            worksheet.write(ligne+1,1, "X")
+            worksheet.write(ligne+1,3, "Y")
+            worksheet.write(ligne+1,5, "Z")
             for i in range(0,3):
-                worksheet.write(2,colonne+2*i, "statistic")
-                worksheet.write(2,colonne+2*i+1, "p-value")
+                worksheet.write(ligne+2,2*i+1, "statistic")
+                worksheet.write(ligne+2,2*i+2, "p-value")
 
-            resultatTests[articulation] = self.testKS(population1.articulations[articulation],population2.articulations[articulation])
-            cellFormatRouge = workbook.add_format({ 'bg_color': '#F08080'})
-            cellFormatVert = workbook.add_format({ 'bg_color': '#90EE90'})
-
-            for i in range(0,3):
-                if resultatTests[articulation][i][1]<0.05:
-                    worksheet.write(3,colonne+2*i,resultatTests[articulation][i][0],cellFormatRouge)
-                    worksheet.write(3,colonne+2*i+1,resultatTests[articulation][i][1],cellFormatRouge)
-                else:
-                    worksheet.write(3,colonne+2*i,resultatTests[articulation][i][0],cellFormatVert)
-                    worksheet.write(3,colonne+2*i+1,resultatTests[articulation][i][1],cellFormatVert)
-
-            colonne +=6
+            resultatTestsPos[articulation] = self.testKS("greater", population1.articulations[articulation],population2.articulations[articulation])
+            resultatTestsNeg[articulation] = self.testKS("less", population1.articulations[articulation],population2.articulations[articulation])
+            self.KSTestWriter(articulation, resultatTestsPos, ligne+3, workbook, worksheet)
+            self.KSTestWriter(articulation, resultatTestsNeg, ligne+4, workbook, worksheet)
+            ligne +=5
         workbook.close()
         print("Test Kolmogorov-Smirnov OK")
-        return resultatTests
+        return resultatTestsPos, resultatTestsNeg
+    
+    def KSTestWriter(self, articulation, resultatTests,ligne, workbook, worksheet):
+        cellFormatRouge = workbook.add_format({ 'bg_color': '#F08080'})
+        cellFormatVert = workbook.add_format({ 'bg_color': '#90EE90'})
+        for i in range(0,3):
+            if resultatTests[articulation][i][1]<0.05:
+                worksheet.write(ligne,2*i+1,resultatTests[articulation][i][0],cellFormatRouge)
+                worksheet.write(ligne,2*i+2,resultatTests[articulation][i][1],cellFormatRouge)
+            else:
+                worksheet.write(ligne,2*i+1,resultatTests[articulation][i][0],cellFormatVert)
+                worksheet.write(ligne,2*i+2,resultatTests[articulation][i][1],cellFormatVert)
 
-    def testKS(self,articulation1, articulation2):
+
+    def testKS(self,sens, articulation1, articulation2):
         resultat=[]
-        resultat.append(scp.ks_2samp(articulation1.moyenneX, articulation2.moyenneX))
-        resultat.append(scp.ks_2samp(articulation1.moyenneY, articulation2.moyenneY))
-        resultat.append(scp.ks_2samp(articulation1.moyenneZ, articulation2.moyenneZ))
+        resultat.append(scp.ks_2samp(articulation1.moyenneX, articulation2.moyenneX, alternative=sens))
+        resultat.append(scp.ks_2samp(articulation1.moyenneY, articulation2.moyenneY, alternative=sens))
+        resultat.append(scp.ks_2samp(articulation1.moyenneZ, articulation2.moyenneZ, alternative=sens))
 
         return resultat
 
-    def generateurDonneesGlobal(self,population,path, KStest=""):
+    def generateurDonneesGlobal(self,population,path, KStestPos="", KStestNeg=""):
         if not os.path.exists(path):
             os.mkdir(path)
         self.generateurDonneesExcel(population, path)
-        self.generateurGraphePopulation(population, path, KStest)
+        self.generateurGraphePopulation(population, path, KStestPos, KStestNeg)
         print("Stats " + population.nom + " OK")
 
     def generateurDonneesExcel(self, population,path):
@@ -108,7 +116,7 @@ class ComparatifPopulations():
         workbook.close()
 
 
-    def generateurGraphePopulation(self, population,path, KStest):
+    def generateurGraphePopulation(self, population,path, KStestPos, KStestNeg):
         correspondancesGraphe = [
             {"cote":"G", "noms": ["Bascule bassin G","Obliquité bassin G","Rotation bassin G"], "articulation": "PelvisG"},
             {"cote":"G", "noms": ["Flexion hanche G","Abd-adduction hanche G","Rotation hanche G"], "articulation": "HancheG"},
@@ -127,27 +135,29 @@ class ComparatifPopulations():
 
             for j in range(0,len(corresp["noms"])):
                 if corresp["noms"][j]!="":
-                    if KStest!='':
-                        KStestEnCours = KStest[corresp["articulation"]][j][1]
+                    if KStestPos!='':
+                        KStestPosEnCours = KStestPos[corresp["articulation"]][j][1]
+                        KStestNegEnCours = KStestNeg[corresp["articulation"]][j][1]
                     else:
-                        KStestEnCours = ""
+                        KStestPosEnCours = ""
+                        KStestNegEnCours = ""
                     if j == 0:
                         if corresp["cote"]=="G":
-                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneX,donnees.stdevX,population.moyennePourcentageContactG,KStestEnCours)
+                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneX,donnees.stdevX,population.moyennePourcentageContactG,KStestPosEnCours, KStestNegEnCours)
                         elif corresp["cote"]=="D":    
-                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneX,donnees.stdevX,population.moyennePourcentageContactD, KStestEnCours)
+                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneX,donnees.stdevX,population.moyennePourcentageContactD, KStestPosEnCours, KStestNegEnCours)
                     elif j == 1:                    
                         if corresp["cote"]=="G":
-                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneY,donnees.stdevY,population.moyennePourcentageContactG, KStestEnCours)
+                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneY,donnees.stdevY,population.moyennePourcentageContactG, KStestPosEnCours, KStestNegEnCours)
                         elif corresp["cote"]=="D":    
-                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneY,donnees.stdevY,population.moyennePourcentageContactD, KStestEnCours)
+                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneY,donnees.stdevY,population.moyennePourcentageContactD, KStestPosEnCours, KStestNegEnCours)
                     elif j == 2:
                         if corresp["cote"]=="G":
-                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneZ,donnees.stdevZ,population.moyennePourcentageContactG, KStestEnCours)
+                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneZ,donnees.stdevZ,population.moyennePourcentageContactG, KStestPosEnCours, KStestNegEnCours)
                         elif corresp["cote"]=="D":            
-                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneZ,donnees.stdevZ,population.moyennePourcentageContactD, KStestEnCours)
+                            self.generateurGrapheSimple(corresp["noms"][j], path, donnees.moyenneZ,donnees.stdevZ,population.moyennePourcentageContactD, KStestPosEnCours, KStestNegEnCours)
 
-    def generateurGrapheSimple(self,nom, path, donnees, donneesStdev,contact, KStest):
+    def generateurGrapheSimple(self,nom, path, donnees, donneesStdev,contact, KStestPos, KStestNeg):
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
         ax.grid(color='black', linestyle='-', linewidth=0.1)
@@ -160,13 +170,19 @@ class ComparatifPopulations():
         minGraph = min(donnees)-max(donneesStdev)*1.1
         maxGraph = max(donnees)+max(donneesStdev)*1.1
         ax.set_ylim(minGraph, maxGraph)
-        if KStest != '':
-            if KStest < 0.05:
-                color = "#F08080"
+        if KStestPos != '':
+            if KStestPos < 0.05:
+                colorPos = "#F08080"
             else :
-                color = "#90EE90"
-            positionTexte = maxGraph-(abs(maxGraph)+abs(minGraph))*0.04
-            ax.text(1, positionTexte,"P-value KS: " + str(format(KStest, '.4f')), bbox=dict(boxstyle="square,pad=0.3",fc=color))
+                colorPos = "#90EE90"
+            if KStestNeg < 0.05:
+                colorNeg = "#F08080"
+            else :
+                colorNeg = "#90EE90"
+            positionTextePos = maxGraph-(abs(maxGraph)+abs(minGraph))*0.04
+            positionTexteNeg = maxGraph-(abs(maxGraph)+abs(minGraph))*0.1
+            ax.text(1, positionTextePos,"P-value KS: " + str(format(KStestPos, '.4f')), bbox=dict(boxstyle="square,pad=0.3",fc=colorPos))
+            ax.text(1, positionTexteNeg,"P-value KS: " + str(format(KStestNeg, '.4f')), bbox=dict(boxstyle="square,pad=0.3",fc=colorNeg))
         #Génère les plots de données et les zone grisées de stDev
         donneesErrPos = []
         donneesErrNeg = []        
